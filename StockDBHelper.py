3a4335bafaf92c0f404cebf1a9b7f21e08dac4c0
LGWayneq <g_wayneliau@yahoo.com.sg>
2021-09-26 17:02:30 +0800

    change DB name, include setAlerts functionality

--- StockDBHelper.py
+++ StockDBHelper.py
@@ -1,37 +1,76 @@
 import sqlite3
 
-class DBHelper:
-    def __init__(self, dbname="gmebottime.sqlite"):
+class DBHelper():
+    def __init__(self, dbname="stockBotDB.sqlite"):
         self.dbname = dbname
-        self.conn = sqlite3.connect(dbname)
+        self.conn = sqlite3.connect(dbname, check_same_thread = False)
     
     def createTable(self):
-        cmd = "CREATE TABLE IF NOT EXISTS reminders (description text, owner text)"
-        cmd2 = "CREATE TABLE IF NOT EXISTS remindertime (owner text PRIMARY KEY, timeinterval int)"
-        ownidx = "CREATE INDEX IF NOT EXISTS ownIndex ON reminders (owner ASC)"
+        cmd = "CREATE TABLE IF NOT EXISTS alerts (owner_id INTEGER PRIMARY KEY, alert BOOLEAN)"
+        cmd2 = "CREATE TABLE IF NOT EXISTS pastTickers (owner_id INTEGER PRIMARY KEY, first_ticker TEXT, second_ticker TEXT, last_ticker TEXT)"
         self.conn.execute(cmd)
         self.conn.execute(cmd2)
-        self.conn.execute(ownidx)
         self.conn.commit()
+        
     
-    def activeChats(self):
-        cmd = "SELECT owner FROM reminders GROUP BY owner"
-        return [x[0] for x in self.conn.execute(cmd)]
-    
-    def setTime(self, owner, time):
-        timeinterval = time
-        cmd = "INSERT OR IGNORE INTO remindertime (owner, timeinterval) VALUES (?, ?, ?)"
-        cmd2 = "UPDATE remindertime SET usertimehr = (?) WHERE owner = (?)"
-        arg = (owner,timeinterval)
-        arg2 = (timeinterval,owner)
-        self.conn.execute(cmd,arg)
-        self.conn.execute(cmd2,arg2)
-        self.conn.commit()
-    
-    def checkTime(self, owner):
-        cmdtime = "SELECT timeinterval FROM remindertime WHERE owner = (?)"
+    def checkAlerts(self, owner):
+        cmd = "SELECT alert FROM alerts WHERE owner_id = (?)"
         arg = (owner, )
-        mins = [x[0] for x in self.conn.execute(cmdtime, arg)]
-        return (mins[0])
+        alerts = [x[0] for x in self.conn.execute(cmd, arg)][0]
+        return alerts
+    
+    def getAllAlerts(self):
+        cmd = "SELECT owner_id FROM alerts WHERE alert = (?)"
+        arg = (1,)
+        alerts = [x[0] for x in self.conn.execute(cmd, arg)]
+        return alerts
+    
+    def setAlerts(self, owner, alert_bool):
+        try:
+            ownerid = [x[0] for x in self.conn.execute("SELECT owner_id FROM alerts WHERE owner_id = (?)", (owner,))][0]
+            cmd = "UPDATE alerts SET alert = (?) WHERE owner_id = (?)"
+            arg = (alert_bool, owner)
+        except:
+            cmd = "INSERT INTO alerts VALUES ((?),(?))"
+            arg = (owner, alert_bool)
+        self.conn.execute(cmd, arg)
+        self.conn.commit()
+    
+        
+    def updatePastTickers(self, owner, ticker):
+        try:
+            ownerid = [x[0] for x in self.conn.execute("SELECT owner_id FROM pastTickers WHERE owner_id = (?)", (owner,))][0]
+            first_ticker = [x[0] for x in self.conn.execute("SELECT first_ticker FROM pastTickers WHERE owner_id = (?)", (owner,))][0]
+        except:
+            cmd = "INSERT INTO pastTickers VALUES((?),(?),(?),(?))"
+            arg = (owner, ticker, None, None)
+            self.conn.execute(cmd, arg)
+            self.conn.commit()
+            return
+        second_ticker = [x[0] for x in self.conn.execute("SELECT second_ticker FROM pastTickers WHERE owner_id = (?)", (owner,))][0]
+        last_ticker = [x[0] for x in self.conn.execute("SELECT last_ticker FROM pastTickers WHERE owner_id = (?)", (owner,))][0]
+        if second_ticker == None and ticker != first_ticker:
+            cmd = "UPDATE pastTickers SET second_ticker = (?) WHERE owner_id = (?)"
+            arg = (ticker, owner)
+        elif last_ticker == None and ticker != first_ticker and ticker != second_ticker:
+            cmd = "UPDATE pastTickers SET last_ticker = (?) WHERE owner_id = (?)"
+            arg = (ticker, owner)      
+        elif ticker != first_ticker and ticker != second_ticker and ticker != last_ticker:
+            cmd = "UPDATE pastTickers SET first_ticker = (?), second_ticker = (?), last_ticker = (?) WHERE owner_id = (?)"
+            arg = (second_ticker, last_ticker, ticker, owner)
+        else:
+            return
+        self.conn.execute(cmd, arg)
+        self.conn.commit()
         
         
+    
+    def getPastTickers(self, owner):
+        try:
+            ownerid = [x[0] for x in self.conn.execute("SELECT owner_id FROM pastTickers WHERE owner_id = (?)", (owner,))][0]
+        except:
+            return []
+        cmd = "SELECT last_ticker, second_ticker, first_ticker FROM pastTickers WHERE owner_id = (?)"
+        arg = (owner,)
+        pasttickers = [x for x in self.conn.execute(cmd, arg)][0]
+        return pasttickers
--- main.py
+++ main.py
@@ -1,61 +1,115 @@
 from StockDBHelper import DBHelper
 import logging
-import os
-from random import randint
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
 from telegram import Bot, ReplyKeyboardMarkup, KeyboardButton
-import pathlib
-import requests
-from bs4 import BeautifulSoup
+import urllib.request
+import json
+import datetime
 
 db = DBHelper()
 TOKEN = '1652839470:AAHwkUcB-5-_eLITVWYRvauhTEr_PgIXNrE'
 URL = 'https://api.telegram.org/bot{}/'.format(TOKEN)
-appname = "https://stockpricebot.herokuapp.com/"
-commandslist = ["/getprice", "/startupdates","/stopupdates","/settimer"]
-
-startmessage = "Type /getprice to get current stock price. The other functions don't work yet."
+bot = Bot(TOKEN)
+#appname = "https://stockpricebot.herokuapp.com/"
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
-availablePorts = ["443","80","88","8443"]
-PORT = int(os.environ.get('PORT', '88'))
+#availablePorts = ["443","80","88","8443"]
+#PORT = int(os.environ.get('PORT', '443'))
 
-bot = Bot(TOKEN)
-
-ziweiid = 245334087
-
-gmeurl = "https://ca.finance.yahoo.com/quote/GME?p=GME&.tsrc=fin-srch"
-
+commandslist = ["/getprice", "/setalerts"]
+with open("startmessage.txt", "r") as startfile:
+    startmessage = startfile.read()
+    startfile.close()
+baseurl = "https://query1.finance.yahoo.com/v8/finance/chart/{}?region=SG&lang=en-SG&includePrePost=false&interval=2m&useYfid=true&range=1d&corsDomain=sg.finance.yahoo.com&.tsrc=finance"
 
 def start(update, context):
     chatid = update.message.chat_id
-    bot.send_message(chat_id = chatid, text = startmessage, reply_markup = createKeyboard())
-    
-def getPrice(update, context):
-    chatid = update.message.chat_id
-    gmepage = requests.get(gmeurl)
-    html = BeautifulSoup(gmepage.text, 'lxml')
-    change = html.find("span", class_ = 'Trsdu(0.3s) Mstart(4px) D(ib) Fz(24px) C($positiveColor)').text
-    price = html.find('span', class_ = 'C($primaryColor) Fz(24px) Fw(b)').text
-    bot.send_message(chat_id = chatid, text = price, reply_markup = createKeyboard())
+    bot.send_message(chat_id = chatid, text = startmessage, reply_markup = createKeyboard(commandslist))
 
-def reply(update, context):
+def get_price_start(update,context):
     chatid = update.message.chat_id
-    chatmessage = update.message.text
-    bot.send_message(chat_id=chatid, text = "test", reply_markup = createKeyboard())    
+    pasttickers = db.getPastTickers(chatid)
+    bot.send_message(chat_id = chatid, text = "Select one of your past three tickers, or input desired ticker\nType /cancel to cancel.", reply_markup = createKeyboard(pasttickers))
+    return 1
+
+def set_ticker(update, context):
+    chatid = update.message.chat_id
+    ticker = update.message.text
+    if ticker == "/cancel":
+        return get_price_cancel(update,context)
+    try:
+        response = urllib.request.urlopen(baseurl.format(ticker))
+        price = json.load(response)["chart"]["result"][0]["meta"]["regularMarketPrice"]    
+        db.updatePastTickers(chatid, ticker.upper())
+        bot.send_message(chat_id = chatid, text = price, reply_markup = createKeyboard(commandslist))
+    except:
+        bot.send_message(chat_id = chatid, text = "Ticker does not exist", reply_markup = createKeyboard(commandslist))
+    return ConversationHandler.END
+
+def get_price_cancel(update,context):
+    chatid = update.message.chat_id
+    bot.send_message(chat_id = chatid, text = "Cancelled /getprice call", reply_markup = createKeyboard(commandslist))
+    return ConversationHandler.END
+
+def set_alert_start(update, context):
+    chatid = update.message.chat_id
+    bot.send_message(chat_id = chatid, text = "Select On to turn on hourly alerts\nSelect Off to turn off hourly alerts\nSelect Cancel to cancel selection", reply_markup = createKeyboard(['On','Off','Cancel']))
+    return 1
+    
+def set_alert_main(update, context):
+    chatid = update.message.chat_id
+    choice = update.message.text
+    if choice == "On":
+        db.setAlerts(chatid, True)
+        bot.send_message(chat_id = chatid, text = "Alerts set to On", reply_markup = createKeyboard(commandslist))
+    elif choice == "Off":
+        db.setAlerts(chatid, False)
+        bot.send_message(chat_id = chatid, text = "Alerts set to Off", reply_markup = createKeyboard(commandslist))
+    elif choice == "Cancel":
+        bot.send_message(chat_id = chatid, text = "Alerts choice selection cancelled", reply_markup = createKeyboard(commandslist))
+    return ConversationHandler.END
+        
+def set_alert_fallback(update, context):
+    chatid = update.message.chat_id
+    choice = update.message.text
+    bot.send_message(chat_id = chatid, text = "Choice of '{}' does not exist. Please try again.".format(choice), reply_markup = createKeyboard(['On','Off','Cancel']))
+    return 1    
+
+def set_alert_cancel(update, context):
+    chatid = update.message.chat_id
+    alert_status = db.checkAlerts(chatid)
+    if alert_status == 1:
+        alert_status = True
+    else:
+        alert_status = False
+    bot.send_message(chat_id = chatid, text = "Set alert cancelled.\nAlerts will remain as {}".format(alert_status), reply_markup = createKeyboard(commandslist))
+    return ConversationHandler.END
+
+def alert_daily(context: CallbackContext):
+    user_list = db.getAllAlerts()
+    for user in user_list:
+        ticker_list = db.getPastTickers(user)
+        full_message = ""
+        for ticker in ticker_list:
+            response = urllib.request.urlopen(baseurl.format(ticker))
+            price = json.load(response)["chart"]["result"][0]["meta"]["regularMarketPrice"]    
+            full_message = full_message + "{}:  {}\n".format(ticker, price)
+        bot.send_message(chat_id = user, text = full_message ,reply_markup = createKeyboard(commandslist))
+    
 
 def error(update, context):
-    """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-def createKeyboard():
+def createKeyboard(keylist):
     button_list = []
-    for each in commandslist:
-       button_list.append(KeyboardButton(each, callback_data = each))
+    for each in keylist:
+        if each == None:
+            continue
+        button_list.append(KeyboardButton(each, callback_data = each))
     reply_markup=ReplyKeyboardMarkup(build_menu(button_list,n_cols=1)) #n_cols = 1 is for single column and mutliple rows
     return reply_markup
     
@@ -68,26 +122,46 @@ def build_menu(buttons,n_cols,header_buttons=None,footer_buttons=None):
     return menu
 
 def main():
+    db.createTable()
     updater = Updater(
         TOKEN, use_context=True)
-
+    
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
+    job = updater.job_queue
 
     dp.add_handler(CommandHandler(["start","help"], start))
-    dp.add_handler(CommandHandler("getprice",getPrice))
-
-    dp.add_handler(MessageHandler(Filters.text, reply))
-
+    
+    setAlertConvHandler = ConversationHandler(
+        entry_points=[CommandHandler('setalerts', set_alert_start)],
+        states = {1: [MessageHandler(Filters.regex(r'^(On|Off|Cancel)?'), set_alert_main)]},
+        fallbacks = [CommandHandler('cancel', set_alert_cancel),
+                     MessageHandler(Filters.text, set_alert_fallback)]
+    )
+    
+    getPriceConvHandler = ConversationHandler(
+        entry_points=[CommandHandler('getprice', get_price_start)],
+        states = {1: [MessageHandler(Filters.text, set_ticker)]},
+        fallbacks = [CommandHandler('cancel', get_price_cancel)]
+    )
+    
+    dp.add_handler(getPriceConvHandler)
+    dp.add_handler(setAlertConvHandler)
+    
+    job.run_daily(alert_daily, days = (0,1,2,3,4), time = datetime.time(hour = 13, minute = 30, second = 10)) #alert for market, time in UTC+0
+    job.run_daily(alert_daily, days = (0,1,2,3,4), time = datetime.time(hour = 19, minute = 30, second = 10))  #alert for market close, time in UTC+0
+    
     # log all errors
     dp.add_error_handler(error)
-
+    
+    updater.start_polling()
+    
     # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",
+    """updater.start_webhook(listen="0.0.0.0",
                           port=PORT,
                           url_path=TOKEN)
-    # updater.bot.set_webhook(url=settings.WEBHOOK_URL)
-    updater.bot.set_webhook(appname + TOKEN)
+    
+    updater.bot.set_webhook(appname + TOKEN)"""
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
